f6ac14e3574ba5478e89668a25c313e7096a7907
MaksymKurny <78872568+MaksymKurny@users.noreply.github.com>
2024-07-25 12:53:53 +0300

    add update file

---
+++ update translate.bat
@@ -0,0 +1,14 @@
+@echo off
+
+set "fileUrl=https://raw.githubusercontent.com/MaksymKurny/Rotwood-UA/main/data_scripts_zip/localizations/uk.po"
+set "fileName=uk.po"
+set "localizationsFolder=localizations"
+
+curl -L -o "%fileName%" "%fileUrl%" >nul 2>&1
+
+if %errorlevel% equ 0 (
+    if not exist "%localizationsFolder%" (
+        mkdir "%localizationsFolder%"
+    )
+    move /Y "%fileName%" "%localizationsFolder%\%fileName%" >nul 2>&1
+)
